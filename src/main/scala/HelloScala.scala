756d1dfcb92790b9abc303d4c59dfdc6660fe580
Khatiwada <kkhati1@students.towson.edu>
2016-09-01 12:00:07 -0400

    Initial commit

---
+++ src/main/scala/HelloScala.scala
@@ -0,0 +1,7 @@
+/**
+  * Created by kkhati1 on 9/1/2016.
+  */
+object HelloScala extends App{
+  println("Hello, world! Hello, Scala!")
+
+}
---
+++ src/main/scala/Lab1.sc
@@ -0,0 +1,4 @@
+println("Hello, Scala!")
+1+4
+var day: Int = 1
+day = 6
